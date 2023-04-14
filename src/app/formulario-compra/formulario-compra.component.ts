e2a323d82c9e04bdd7cc3f24025aa38bfee6cc94
Ricardo Moreno-Ventas <ricardo.morenoventas@softtek.com>
2023-04-14 11:59:16 +0200

    Eliminado form y uso de 2WayBinding

--- src/app/formulario-compra/formulario-compra.component.ts
+++ src/app/formulario-compra/formulario-compra.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-compra',
@@ -8,20 +7,61 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class FormularioCompraComponent {
 
-  form: FormGroup = this.fb.group({
-    comunidad_seleccionada : "Madrid",
-    direccion: "",
-    email_intro: "",
-    forma_pago: "",
-    telefono_introducido: ""
-  });
+  // Atributos
+  private _comunidad_seleccionada: string = "Madrid";
+  private _direccion: string = "";
+  private _email_intro: string = "";
+  private _forma_pago: string = "Efectivo";
+  private _telefono_introducido: string = "";
+  private _aceptado: boolean = false;
 
-  aceptado:boolean = false;
+  constructor(){}
 
-  changeBool(a:void) : void{
-    this.aceptado = !this.aceptado;
+  // Getters & Setters
+  public get comunidad_seleccionada(): string {
+    return this._comunidad_seleccionada;
+  }
+  public set comunidad_seleccionada(value: string) {
+    this._comunidad_seleccionada = value;
   }
 
-  constructor(private fb: FormBuilder){}
+  public get direccion(): string {
+    return this._direccion;
+  }
+  public set direccion(value: string) {
+    this._direccion = value;
+  }
 
+  public get email_intro(): string {
+    return this._email_intro;
+  }
+  public set email_intro(value: string) {
+    this._email_intro = value;
+  }
+
+  public get forma_pago(): string {
+    return this._forma_pago;
+  }
+  public set forma_pago(value: string) {
+    this._forma_pago = value;
+  }
+
+  public get telefono_introducido(): string {
+    return this._telefono_introducido;
+  }
+  public set telefono_introducido(value: string) {
+    this._telefono_introducido = value;
+  }
+
+  public get aceptado(): boolean {
+    return this._aceptado;
+  }
+  public set aceptado(value: boolean) {
+    this._aceptado = value;
+  }
+
+  // Métodos
+  changeBool(a:void) : void{
+    this._aceptado = !this._aceptado;
+  }
 }
